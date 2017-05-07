7f9f8cfacf28575e967865478d4d399bbc2f4569
geraJuarez <geratrex781@gmail.com>
2017-05-06 18:32:18 -0600

    Completed Heun's method and some Euler changes

--- Diferential Equations/Euler.cpp
+++ Diferential Equations/Euler.cpp
@@ -8,24 +8,33 @@ double df( double x ) {
     return - ( 2* pow (x, 3 ) ) + ( 12 * pow( x, 2 ) ) - ( 20 * x ) + 8.5;
 }
 
-double euler( double x0, double y0, double xDeseada ) {
+double euler( double x0, double y0, double xDeseada, double h ) {
 
-    double h = 0.5;     //Intervalo
     double y1;
 
     while ( x0 < xDeseada ) {
 
-        cout << "x:" << x0 << " y:" << y0 << endl;
+        cout << "x:" << x0 << "\ty:" << y0 << endl;
         y1 = y0 + ( df( x0 ) * h );
         x0 += h;
         y0 = y1;
     }
-    cout << "\ny at " << xDeseada << ": " << y0 << endl;
+    cout << "\ny at " << xDeseada << " = " << y0 << endl;
 }
 
 int main() {
 
-    euler( 0, 1, 4 );
+    double x;
+    double y;
+    double h;   //Intervalo
+    double x_deseada;
+
+    cout << "Initial x: ";  cin >> x;
+    cout << "Initial y: ";  cin >> y;
+    cout << "Wanted x: ";   cin >> x_deseada;
+    cout << "Steps: ";      cin >> h;
+
+    euler( x, y, x_deseada, h );
 
     return 0;
 }
--- Diferential Equations/Heun.cpp
+++ Diferential Equations/Heun.cpp
@@ -7,26 +7,45 @@ double df( double x, double y ) {
     return 4 * exp ( 0.8 * x ) - 0.5 * y;
 }
 
-double euler( double x0, double y0 ) {
+double euler( double x0, double y0, double h ) {
 
-    double h = 0.5;     //Intervalo
     double y1 = 0;
     y1 = y0 + df( x0, y0 ) * h;
 
     return y1;
 }
 
-double Heun( double x0, double y0 ) {
+void Heun( double x0, double y0, double h, double x_deseada ) {
+
+    cout << "**** Heun's Method ****" << endl;
 
     double y1 = 0;
-    y1 = y0 + ( df( x0, y0 ) +  df( x0 + 1, euler(x0, y0) ) * h ) / 2;
 
-    cout << y1 << endl;
+    while ( x0 < x_deseada ) {
+
+        y1 = y0 + ( df( x0, y0 ) +  df( x0 + 1, euler(x0, y0, h) ) * h ) / 2;
+        cout << y1 << endl;
+        x0 += h;
+        y0 = y1;
+    }
+
+    cout << "\ny_Heun = " << y0 << endl;
+    
 }
 
 int main() {
 
-    Heun( 0, 2 );
+    double x;
+    double y;
+    double h;   //Intervalo
+    double x_deseada;
+
+    cout << "Initial x: ";  cin >> x;
+    cout << "Initial y: ";  cin >> y;
+    cout << "Wanted x: ";   cin >> x_deseada;
+    cout << "Steps: ";      cin >> h;
+
+    Heun( x, y, h, x_deseada );
 
     return 0;
 }
