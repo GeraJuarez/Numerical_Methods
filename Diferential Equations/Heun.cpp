8afab7cd8b757f06b36cf43dc98cceaa2d1d8e99
geraJuarez <geratrex781@gmail.com>
2017-05-06 18:43:34 -0600

    Minor changes

--- Diferential Equations/Heun.cpp
+++ Diferential Equations/Heun.cpp
@@ -45,7 +45,36 @@ int main() {
     cout << "Wanted x: ";   cin >> x_deseada;
     cout << "Steps: ";      cin >> h;
 
-    Heun( x, y, h, x_deseada );
+    //Heun( x, y, h, x_deseada );
+    Midpoint( x, y, h, x_deseada );
 
     return 0;
 }
+
+/* Under development
+double euler2( double x0, double y0, double h ) {
+
+    double y1 = 0;
+    y1 = y0 + df( x0, y0 ) * h / 2;
+
+    return y1;
+}
+
+void Midpoint( double x0, double y0, double h, double x_deseada ) {
+
+    cout << "**** Midpoint or improved polygon Method ****" << endl;
+
+    double y1 = 0;
+
+    while ( x0 < x_deseada ) {
+
+        y1 = y0 + ( df( x0, y0 ) +  df( x0 + 1, euler2(x0, y0, h) ) * h / 2 );
+        cout << y1 << endl;
+        x0 += h;
+        y0 = y1;
+    }
+
+    cout << "\ny_Heun = " << y0 << endl;
+    
+}
+*/
